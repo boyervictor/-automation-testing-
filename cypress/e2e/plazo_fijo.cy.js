ab2caf8ff2f423f0f1f9a6b72b5be85af5d189ab
boyervictor <54690058+boyervictor@users.noreply.github.com>
2025-05-23 14:30:04 -0300

    Update plazo_fijo.cy.js
    casos de prueba positivos para el módulo de Plazo Fijo en un entorno bancario web

--- cypress/e2e/plazo_fijo.cy.js
+++ cypress/e2e/plazo_fijo.cy.js
@@ -1,1 +1,44 @@
+describe('Módulo de Plazo Fijo – Casos Positivos', () => {
+  
+  beforeEach(() => {
+    cy.visit('https://tubanco.com/login');
+    cy.get('#usuario').type('cliente123');
+    cy.get('#password').type('claveSegura!');
+    cy.get('#login-button').click();
+    cy.url().should('include', '/dashboard');
+  });
+
+  it('Crear un plazo fijo tradicional en pesos con renovación total', () => {
+    cy.get('#menu-plazo-fijo').click();
+    cy.get('#tipo-moneda').select('Pesos');
+    cy.get('#monto').type('100000');
+    cy.get('#plazo').select('30 días');
+    cy.get('#tipo-renovacion').select('Total');
+    cy.get('#confirmar').click();
+
+    cy.get('.alert-success')
+      .should('contain', 'Plazo fijo creado exitosamente');
+  });
+
+  it('Crear un plazo fijo en dólares sin renovación', () => {
+    cy.get('#menu-plazo-fijo').click();
+    cy.get('#tipo-moneda').select('Dólares');
+    cy.get('#monto').type('1500');
+    cy.get('#plazo').select('60 días');
+    cy.get('#tipo-renovacion').select('Sin renovación');
+    cy.get('#confirmar').click();
+
+    cy.get('.alert-success')
+      .should('contain', 'Plazo fijo creado exitosamente');
+  });
+
+  it('Visualizar listado de plazos fijos vigentes', () => {
+    cy.get('#menu-plazo-fijo').click();
+    cy.get('#ver-plazos-fijos').click();
+
+    cy.get('table#plazos-fijos tbody tr').should('have.length.at.least', 1);
+    cy.get('table#plazos-fijos').should('contain', 'Pesos')
+                                 .and('contain', 'Dólares');
+  });
+});
 
